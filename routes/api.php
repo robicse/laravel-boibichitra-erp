4d0d0d52b9d86a98f13a706c800fc8bc6d5b4baf
Md. Robeul Islam <robeul.starit@gmail.com>
2021-03-09 19:18:36 +0600

    api

--- routes/api.php
+++ routes/api.php
@@ -422,6 +422,6 @@ Route::middleware('auth:api')->post('/ledger', 'API\BackendController@ledger');
 Route::middleware('auth:api')->post('/balance_sheet', 'API\BackendController@balanceSheet');
 
 // database backup
-//Route::get('/backup_database', 'HomeController@backup_database')->name('backup_database');
-Route::middleware('auth:api')->get('/backup_database', 'API\BackendController@backupDatabase');
+Route::get('/backup_database', 'HomeController@backup_database')->name('backup_database');
+//Route::middleware('auth:api')->get('/backup_database', 'API\BackendController@backupDatabase');
 //Route::middleware('auth:api')->get('/sum_sub_total', 'API\BackendController@sum_sub_total');
